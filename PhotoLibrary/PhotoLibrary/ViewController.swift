fa2d3c0fa9e98c1a6102e3c690f3747e7abaa33f
JYP <jypjoy@gmail.com>
2021-08-26 20:28:28 +0900

    Add PhotoLibrary

---
+++ PhotoLibrary/PhotoLibrary/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  PhotoLibrary
+//
+//  Created by Jiyoung Park on 2021/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapButton() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary //.camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate,
+                          UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            imageView.image = image
+        }
+    
+        //print("\(info)")
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
